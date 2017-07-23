38e4e3e595c65e1ca3efcec10d0fecfb02ff6d83
ankurkhemani <ankheman@microsoft.com>
2017-07-23 02:26:44 -0700

    changing port to 80 for publishing

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -2,7 +2,7 @@
 var restify = require('restify');
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 80, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 //server.listen(3978, function () {
