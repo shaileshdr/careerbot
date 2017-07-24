ed759973225b72b5434333b1615984c56af97b23
ankurkhemani <ankheman@microsoft.com>
2017-07-24 13:14:17 -0700

    edited path for images

--- app.js
+++ app.js
@@ -246,7 +246,7 @@ function createCard(session, value, tag) {
         .subtitle(title)
         .text(txt)
         .images([
-            builder.CardImage.create(session, '/images/' + value +'.jpg')
+            builder.CardImage.create(session, '/Images/' + value +'.jpg')
         ])
         .buttons([
             builder.CardAction.openUrl(session, 'https://www.linkedin.com/in/ankurkhemani/', 'View Linked In profile'),
@@ -292,7 +292,7 @@ function createTrainingCard(session, value, tag) {
         .title(title)
         .subtitle(txt)
         .images([
-            builder.CardImage.create(session, '/images/' + value + '.png')
+            builder.CardImage.create(session, '/Images/' + value + '.png')
         ])
         .buttons([
             builder.CardAction.openUrl(session, url, 'View course details'),
